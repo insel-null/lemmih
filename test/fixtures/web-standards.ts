94fdf64b241522ecb2cf04950f0376b1c48ff84b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-11-21 19:11:52 +0800

    chore: add example

---
+++ test/fixtures/web-standards.ts
@@ -0,0 +1,12 @@
+import { App, res, routing } from '../../src'
+
+const app = new App()
+  .route('/', routing.get(res.text('Hello LeMMIH')))
+  .route('/user/:id', routing.get((_, params) => res.text(params!.id!)))
+  .route('/form', routing.post(async req => res.json(await req.json() as Record<string, unknown>)))
+
+// eslint-disable-next-line @masknet/no-top-level
+Bun.serve({
+  fetch: app.fetch,
+  port: 3000,
+})
