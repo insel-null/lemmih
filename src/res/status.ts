3a199617ad4d657af0b94c81f18fe47b9a782bb1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-11-21 19:27:32 +0800

    refactor: inline deps

--- src/res/status.ts
+++ src/res/status.ts
@@ -1,5 +1,5 @@
-import { STATUS_CODES } from 'node:http'
+import { STATUS_CODES } from '../deps/status-codes'
 
-export const status = (status: number) => new Response(STATUS_CODES[status], { status })
+export const status = (status: number) => new Response(STATUS_CODES[status as keyof typeof STATUS_CODES], { status })
 
 export const notFound = () => status(404)
