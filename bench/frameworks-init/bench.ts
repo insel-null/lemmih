8f4db4f603bba202f3a6b2bb79741117821eccf1
SukkaW <isukkaw@gmail.com>
2025-11-23 20:56:30 +0800

    chore(bench/frameworksinit): add summary

--- bench/frameworks-init/bench.ts
+++ bench/frameworks-init/bench.ts
@@ -1,16 +1,18 @@
 /* eslint-disable @masknet/no-top-level */
-import { bench, run } from 'mitata'
+import { bench, run, summary } from 'mitata'
 
 import { elysia } from './elysia'
 import { hono, honoQuick, honoTiny } from './hono'
 import { lemmih, lemmihBun } from './lemmih'
 
-bench('elysia', () => elysia())
-bench('hono', () => hono())
-bench('hono/quick', () => honoQuick())
-bench('hono/tiny', () => honoTiny())
-bench('lemmih', () => lemmih())
-bench('lemmih/bun', () => lemmihBun())
+summary(() => {
+  bench('elysia', () => elysia())
+  bench('hono', () => hono())
+  bench('hono/quick', () => honoQuick())
+  bench('hono/tiny', () => honoTiny())
+  bench('lemmih', () => lemmih())
+  bench('lemmih/bun', () => lemmihBun())
+})
 
 // eslint-disable-next-line antfu/no-top-level-await
 await run()
