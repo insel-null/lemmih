57c108c6281978a730fcc3707eb302ca548383e8
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-11-24 10:26:35 +0800

    chore: name correction

--- src/routing/index.ts
+++ src/routing/index.ts
@@ -8,7 +8,7 @@ export const connect = <T>(handler: Handler<T>) => new MethodRouter<T>().connect
 export const del = <T>(handler: Handler<T>) => new MethodRouter<T>().delete(handler)
 export const get = <T>(handler: Handler<T>) => new MethodRouter<T>().get(handler)
 export const head = <T>(handler: Handler<T>) => new MethodRouter<T>().head(handler)
-export const on = <T>(methods: Method, handler: Handler<T>) => new MethodRouter<T>().on(methods, handler)
+export const on = <T>(method: Method, handler: Handler<T>) => new MethodRouter<T>().on(method, handler)
 export const patch = <T>(handler: Handler<T>) => new MethodRouter<T>().patch(handler)
 export const post = <T>(handler: Handler<T>) => new MethodRouter<T>().post(handler)
 export const put = <T>(handler: Handler<T>) => new MethodRouter<T>().put(handler)
