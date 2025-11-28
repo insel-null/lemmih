e69ca1c8d236ca131bdad7df403434a393b53084
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-11-28 14:23:14 +0800

    refactor(routing): update

---
+++ src/utils/routing.ts
@@ -0,0 +1,19 @@
+import type { Method } from '../core/method-router'
+import type { Handler } from '../core/types'
+
+import { MethodRouter } from '../core/method-router'
+
+const routing = {
+  any: <T>(handler: Handler<T>) => new MethodRouter<T>().any(handler),
+  connect: <T>(handler: Handler<T>) => new MethodRouter<T>().connect(handler),
+  delete: <T>(handler: Handler<T>) => new MethodRouter<T>().delete(handler),
+  get: <T>(handler: Handler<T>) => new MethodRouter<T>().get(handler),
+  head: <T>(handler: Handler<T>) => new MethodRouter<T>().head(handler),
+  on: <T>(method: Method, handler: Handler<T>) => new MethodRouter<T>().on(method, handler),
+  patch: <T>(handler: Handler<T>) => new MethodRouter<T>().patch(handler),
+  post: <T>(handler: Handler<T>) => new MethodRouter<T>().post(handler),
+  put: <T>(handler: Handler<T>) => new MethodRouter<T>().put(handler),
+  trace: <T>(handler: Handler<T>) => new MethodRouter<T>().trace(handler),
+}
+
+export default routing
