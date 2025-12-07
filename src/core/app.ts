44148ecf41b2b23d349e9d8267dacd9c8575c0da
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-12-07 15:37:52 +0800

    refactor: split bun-app, params (#7)

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,15 +1,11 @@
-import type { Handler, Layer, StrictHandler } from './types'
+import type { Handler, StrictHandler } from './types'
 import type { TypedParams } from './types/typed-params'
 
-import { status } from '../res'
+import { BaseApp } from './base-app'
 import { MethodRouter } from './method-router'
 import { PathRouter } from './path-router'
 
-export class App {
-  fallback?: StrictHandler
-  layers: Layer[] = []
-  routesMap: Map<string, Handler> = new Map()
-
+export class App extends BaseApp {
   get fetch(): StrictHandler {
     const router = this.buildRouter()
 
@@ -26,56 +22,16 @@ export class App {
 
   private router: PathRouter<Handler> | undefined
 
-  constructor(fallback?: StrictHandler) {
-    // eslint-disable-next-line @masknet/prefer-early-return
-    if (fallback != null)
-      this.fallback = fallback
-  }
-
-  public async handle(req: Request, handler?: Handler, params?: Record<string, string>): Promise<Response> {
-    const next = async (req: Request) => (handler ?? this.fallback ?? (() => status(404)) satisfies Handler)(
-      req,
-      params,
-    )
-
-    return (
-      this.layers.length > 0
-        ? this.layers.reduceRight<StrictHandler>(
-            (next, layer) => async req => layer(req, next),
-            next,
-          )
-        : next
-    )(req)
-  }
-
-  public layer(...layers: Layer[]): this {
-    this.layers.push(...layers)
-
-    return this
-  }
-
-  public merge(app: App): this {
-    app.routesMap.forEach((handler, path) => this.routesMap.set(path, handler))
-    app.layers.forEach(layer => this.layers.push(layer))
-
-    if (app.fallback) {
-      if (!this.fallback)
-        this.fallback = app.fallback
-      else
-        throw new Error('lemmih: Cannot merge two `App`s that both have a fallback')
-    }
-
+  override merge(app: App): this {
     this.router = undefined
-    return this
+    return super.merge(app)
   }
 
   public route<T extends string>(path: T, handler: Handler<TypedParams<T>> | MethodRouter<TypedParams<T>>): this {
     this.routesMap.set(
       path,
       handler instanceof MethodRouter
-        // TODO: remove any
-        // eslint-disable-next-line ts/no-unsafe-argument
-        ? async (req, params) => handler.handle(req, params as any)
+        ? (async (req, params) => handler.handle(req, params)) satisfies Handler<TypedParams<T>> as Handler
         // eslint-disable-next-line @masknet/type-no-force-cast-via-top-type
         : handler as unknown as Handler,
     )
